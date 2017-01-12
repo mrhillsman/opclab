48fdc4ca2cb584961fbcd67a78b5345003348c74
Melvin Hillsman <mrhillsman@gmail.com>
2017-01-11 23:45:06 -0600

    House cleaning
    Removing unnecessary files, renaming files, and cleaning up some code

--- deploy_phase.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i hosts prepare_for_osa.yaml
-ansible-playbook -i hosts deploy_osa.yaml
--- destroy_phase.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i hosts destroy_virtual_machines.yaml
-ansible-playbook -i hosts destroy_virtual_networks.yaml
-ansible-playbook -i hosts destroy_lab_state_file.yaml
-# Tag needs to be the same as used when building
-ansible-playbook -i hosts destroy_onmetal.yaml --tags 'iad'
